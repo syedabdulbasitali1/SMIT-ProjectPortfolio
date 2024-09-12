2598d1aa0eb635f38214847e6f2dcabe26a4fa46
Abdul Basit Ali <syedabdulbasitali1@gmail.com>
2024-09-12 18:15:20 +0000

    Thu, Sep 12, 2024, 11:15 AM -07:00

--- hello.c
+++ hello.c
@@ -3,32 +3,40 @@
 #include <cs50.h>
 
 int main(void){
+
+// function decleare but values not assigned yet
+void meow(void);
+
     printf("hello, world\n");
-    string answer =get_string("whar's your name?");
-    printf("hello, %s\n",answer);
+    // string answer =get_string("whar's your name?");
+    // printf("hello, %s\n",answer);
 
 
 
 
-int x = get_int("what's x?");
-int y = get_int("what's y?");
+// int x = get_int("what's x?");
+// int y = get_int("what's y?");
 
-if (x < y){
-    printf("x is less than y\n");
-}
-else if(x > y){
-    printf("x is not less than y\n");
-}
-else{
-    printf("x is equal to y\n");
-}
+// if (x < y){
+//     printf("x is less than y\n");
+// }
+// else if(x > y){
+//     printf("x is not less than y\n");
+// }
+// else{
+//     printf("x is equal to y\n");
+// }
 
 // for increament the number
-y += 1;
-y++;
+// y += 1;
+// y++;
 
-// for (i =0; i <3;i++){
-//     printf("meow!\n")
-// }
+for (i = 0; i < 3;i++){
+    printf("meow!\n");
+}
+
+void meow(void){
+    printf("meow!\n");
+}
 
 }
