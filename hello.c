d68bfbb64e9647d980f40daee8be3ed856531891
Abdul Basit Ali <syedabdulbasitali1@gmail.com>
2024-09-12 18:10:38 +0000

    Thu, Sep 12, 2024, 11:10 AM -07:00

--- hello.c
+++ hello.c
@@ -17,10 +17,10 @@ if (x < y){
     printf("x is less than y\n");
 }
 else if(x > y){
-    printf("x is not less than y");
+    printf("x is not less than y\n");
 }
 else{
-    printf("x is equal to y");
+    printf("x is equal to y\n");
 }
 
 // for increament the number
