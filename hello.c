fb20ea28d57c90a367fa527ea5f6251bcbcb9121
Abdul Basit Ali <syedabdulbasitali1@gmail.com>
2024-09-12 18:03:11 +0000

    Thu, Sep 12, 2024, 11:03 AM -07:00

--- hello.c
+++ hello.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
+// cs50 custom header file
+#include <cs50.h>
 
 int main(void){
     printf("hello, world\n");
+    string answer =get_string("whar's your name?");
+    printf("hello, %s\n",answer);
 }
+
+int x = 10;
+int y = 13;
+
+// int x = get_int("what's x?");
+// int y = get_int("what's y?");
+
+if (x < y){
+    printf("x is less than y");
+}
+else if(x > y){
+    printf("x is not less than y");
+}
+else{
+    printf("x is equal to y");
+}
+
+// for increament the number
+y += 1;
+y++;
+
