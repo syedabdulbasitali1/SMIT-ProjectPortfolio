7e10b770f63e8a03b90d6b1094cb632bcac0aa30
Abdul Basit Ali <syedabdulbasitali1@gmail.com>
2024-09-13 05:13:35 +0000

    Thu, Sep 12, 2024, 10:13 PM -07:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -16,8 +16,8 @@ int main(void)
 
     for(int i = 0; i < height; i++)
     {
-        height-=2;
-        for(int k = height; k > 0; k--)
+        height--;
+        for(int k = height-1; k > 0; k--)
             {
                 printf(".");
 
