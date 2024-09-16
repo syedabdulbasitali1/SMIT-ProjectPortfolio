6623d519a14622f0a3532a7e36b7a86a4245adce
Abdul Basit Ali <syedabdulbasitali1@gmail.com>
2024-09-16 05:37:13 +0000

    Sun, Sep 15, 2024, 10:37 PM -07:00

--- cash/cash.c
+++ cash/cash.c
@@ -1,4 +1,45 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int return(int cashback)
+
 int main()
+{
+    int cash;
+    do{
+        int cash = get_int("inter amount");
+    }
+    while(cash < 1);
+
+    int result =return(cash);
+    printf(result);
+
+
+
+}
+
+
+int return(int cashback);
+{
+    int n;
+    while(cashback > 25){
+        n += (cashback-25);
+        n++;
+    }
+    while(cashback > 10)
+    {
+        n += (cashback - 10);
+        n++;
+    }
+    while(cashback > 5)
+    {
+        n += (cashback -5);
+        n++;
+    }
+    while(cashback > 1)
+    {
+        n += (cashback -1);
+        n++;
+    }
+    return n;
+}
