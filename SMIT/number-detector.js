5c0947d681dc984eb0c0a5691dcd5f1558817e3e
Abdul Basit Ali <syedabdulbasitali1@gmail.com>
2024-09-22 04:38:50 +0000

    Sat, Sep 21, 2024, 9:38 PM -07:00

--- SMIT/number-detector.js
+++ SMIT/number-detector.js
@@ -3,6 +3,13 @@ console.log("Hello, Javascript");
 // If you want to run this code in browser you need to link this file in you HTML than uncomment below prompt
 // let userInput = prompt("Enter a number");
 
+// Use below code if you want to run code in terminal with Node.js
+// First you need to use Node.js module to take userInput in terminal
+// 1. Import the readline module:
+const readline = require('readline');
+
+// 2. Create an interface for reading input:
+// This setup an interface to read from standard
 
 
 if(userInput % 2 === 0){
