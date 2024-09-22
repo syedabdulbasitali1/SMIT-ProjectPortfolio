8fac39ca81f9a1c1f3e6b5903783de54cb781314
Abdul Basit Ali <syedabdulbasitali1@gmail.com>
2024-09-22 04:56:04 +0000

    Sat, Sep 21, 2024, 9:56 PM -07:00

--- SMIT/number-detector.js
+++ SMIT/number-detector.js
@@ -1,26 +1,30 @@
 console.log("Hello, Javascript");
 
-// If you want to run this code in browser you need to link this file in you HTML than uncomment below prompt
-// let userInput = prompt("Enter a number");
 
 // Use below code if you want to run code in terminal with Node.js
 // First you need to use Node.js module to take userInput in terminal
 // 1. Import the readline module:
-const readline = require('readline');
+// const readline = require('readline');
 
 // 2. Create an interface for reading input:
 // This setup an interface to read from standard
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
-// 3. Prompt user input
-rl.question('Enter a string:',
-(answer) => {
-    console.log('You entered: ${answer}');
-    rl.close();
-});
+// // 3. Prompt user input
+// rl.question('Enter a string:',
+// (answer) => {
+//     console.log('You entered: ${answer}');
+//     rl.close();
+// });
+
+
+// If you want to run this code in browser you need to link this file in you HTML than uncomment below prompt
+// also you can run this code in terminal with VS-code extention called code run.
+let userInput = 64;
+// prompt("Enter a number");
 
 if(userInput % 2 === 0){
     console.log("Your number is EVEN.");
@@ -28,4 +32,3 @@ if(userInput % 2 === 0){
 else{
     console.log("Your number is ODD.");
 }
-console.log(31 % 2);
