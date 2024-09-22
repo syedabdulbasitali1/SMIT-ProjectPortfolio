dbf04e44b87c6cc4834d6421baff6f25e53b033b
Abdul Basit Ali <syedabdulbasitali1@gmail.com>
2024-09-22 04:21:24 +0000

    Sat, Sep 21, 2024, 9:21 PM -07:00

--- SMIT/number-detector.js
+++ SMIT/number-detector.js
@@ -2,7 +2,10 @@ console.log("Hello, Javascript");
 
 let userInput = 20;
 
-// if(userInput % 2 == 0){
-
-// }
-console.log(22 % 2);
+if(userInput % 2 === 0){
+    console.log("Your number is EVEN.");
+}
+else{
+    console.log("Your number is ODD.");
+}
+console.log(31 % 2);
