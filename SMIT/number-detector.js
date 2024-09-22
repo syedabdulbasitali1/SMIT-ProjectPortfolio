9eca8a71f994a277f0bbbcb01eb1cfc753fc52cc
Abdul Basit Ali <syedabdulbasitali1@gmail.com>
2024-09-22 04:32:44 +0000

    Sat, Sep 21, 2024, 9:32 PM -07:00

--- SMIT/number-detector.js
+++ SMIT/number-detector.js
@@ -1,6 +1,9 @@
 console.log("Hello, Javascript");
 
+// If you want to run this code in browser you need to link this file in you HTML than uncomment below prompt
 // let userInput = prompt("Enter a number");
+
+
 
 if(userInput % 2 === 0){
     console.log("Your number is EVEN.");
