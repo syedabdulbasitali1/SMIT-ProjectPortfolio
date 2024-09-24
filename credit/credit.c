222ac6eb1710c42e64bb68ee5e46b07f0d5682a9
Abdul Basit Ali <syedabdulbasitali1@gmail.com>
2024-09-24 21:29:53 +0000

    Tue, Sep 24, 2024, 2:29 PM -07:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,13 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int func(int n){
-    return n % 10;
-}
+// int func(int n){
+//     return n % 10;
+// }
 
 int toString(int creditNumber){
     char str[20];
-    sprintf()
+    sprintf(str,"%ld",creditNumber);
 
 }
 int main()
@@ -18,7 +18,8 @@ int main()
         user_input = get_long("Enter credit card number: ");
     }
     while(user_input.length < 1);
-    int result =func(user_input);
+    
+    // int result =func(user_input);
     printf("%i\n", result);
 
 }
