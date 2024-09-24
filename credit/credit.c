756bb1e16cee7b28e6892881e3ad6f6a94c3458b
Abdul Basit Ali <syedabdulbasitali1@gmail.com>
2024-09-24 23:29:10 +0000

    Tue, Sep 24, 2024, 4:29 PM -07:00

--- credit/credit.c
+++ credit/credit.c
@@ -27,8 +27,8 @@ int toString(long creditNumber){
     else if((length == 16 || length == 13) && one == '4' )
         printf("Visa\n");
     else
-    printf("check is invalid");
-    
+    printf("check is invalid\n");
+
     }
 
     else{
